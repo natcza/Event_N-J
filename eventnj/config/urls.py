4d0c2c2f82d4b49c1b4702cfb25e2e48eb480488
jz-cl <janusz.zychowicz@gmail.com>
2022-02-11 23:21:54 +0100

    add view and url dashboard

--- eventnj/config/urls.py
+++ eventnj/config/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-# from events.views import DashboardView
+from events.views import DashboardView
 #
 #
 urlpatterns = [
     path('admin/', admin.site.urls),
-#     path('/', DashboardView.as_view(), name="dashboard"),
+    path('', DashboardView.as_view(), name="dashboard"),
 ]
