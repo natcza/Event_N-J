932d0b45060bfe46b852a0372be47e0b83b0b62e
natcza <natalia.nc.czajkowska@gmail.com>
2022-02-17 21:35:00 +0100

    EventView_EventDetail_update

--- eventnj/config/urls.py
+++ eventnj/config/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-# from events.views import DashboardView
-#
-#
+from events.views import (
+    EventView,
+    EventDetailsView,
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-#     path('/', DashboardView.as_view(), name="dashboard"),
+    path('event/', EventView.as_view(), name="event-list"),
+    path('event-details/<int:pk>/', EventDetailsView.as_view(), name='event-details'),
 ]
--- eventnj/events/views.py
+++ eventnj/events/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from .models import Event
 
 
 # Create your views here.
 
-# class DashboardView(View):
-#     template_name = "events/dashboard.html"
-#
-#     def get(self, request, *args, **kwargs):
-#         events = Event.objects.all().order_by("start")
-#         ctx = {
-#             "events": events,
-#         }
-#         return render(request, self.template_name, ctx)
+class EventView(View):
+    """Funkcja wyświetlająca liste eventów"""
+    template_name = "events/dashboard.html"
+
+    def get(self, request, *args, **kwargs):
+        events = Event.objects.all().order_by("start")
+        ctx = {
+            "events": events,
+        }
+        return render(request, self.template_name, ctx)
+
+class EventDetailsView(View):
+    """Funckcja wyświetlająca opis jednego eventu"""
+    template_name = "events/event_details_view.html"
+
+    def get(self, request, *args, **kwargs):
+        event_id = kwargs["pk"]
+        event = get_object_or_404(Event, pk=event_id)
+        ctx = {
+            "event": event
+        }
+        return render(request, self.template_name, ctx)
