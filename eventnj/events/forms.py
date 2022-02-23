40081b375e320c67c119225c3a302d2eebd03ac0
jz-cl <janusz.zychowicz@gmail.com>
2022-02-23 01:22:33 +0100

    update accept_rodo

--- eventnj/events/forms.py
+++ eventnj/events/forms.py
@@ -5,7 +5,7 @@ from django.forms import widgets
 class addParticipantForm(forms.Form):
     name = forms.CharField(label="Imię ", max_length=255)
     mail = forms.CharField(label="email ", max_length=255)
-    accept_rodo = forms.BooleanField(required=True, error_messages={'required': 'zaakceptuj warunki rodo'})
+    accept_rodo = forms.BooleanField()
 
     # https://www.youtube.com/watch?v=pFMrOpDs4QQ
     # accept_rodo = forms.BooleanField(label='accept rodo', help_text='zaakceptuj warunki rodo')
